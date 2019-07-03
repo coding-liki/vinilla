d9aa3627f01467d8ef1141f7076f5ff1db8a6367
lev <coding1liki@gmail.com>
2019-07-04 01:27:38 +0300

    Добавил update cache

--- php/vinilla.php
+++ php/vinilla.php
@@ -1,6 +1,12 @@
 <?php
 define("SETTINGS_FILE", "vinilla.json");
 define("TMP_DIR", "/tmp/vinilla_install_temp");
+define("SERVER_URL", "http://vinillaserver.vinylcoding.ru");
+define("MODULE_NOT_INSTALLED", 0);
+define("MODULE_INSTALLED_AND_VINILLA", 1);
+define("MODULE_INSTALLED_NOT_VINILLA", 2);
+define("CURRENT_WORKIN_DIR", getcwd());
+
 require_once __DIR__."/include.php";
 /**
  * Проверяем и создаём, если нет, папку vendor в текущей папке
@@ -13,9 +19,10 @@ function checkRootPath(){
 
 function checkTmpFolder(){
     if(checkCreateFolder(TMP_DIR)){
-        echo "we have tmp folder so deleting it\n";
-        deleteDir(TMP_DIR);
-        checkTmpFolder();
+        // echo "we have tmp folder so deleting it\n";
+        // deleteDir(TMP_DIR);
+        // checkTmpFolder();
+        echo TMP_DIR." already exists\n";
     }
 }
 
@@ -24,6 +31,21 @@ function checkVendorFolder($vendor, $work_folder){
     return $work_folder.'/vendor/'.$vendor;
 }
 
+function gitFetchModuleToTmp($module_url){
+    chdir(TMP_DIR);
+    $module_name = explode("/", $module_url);
+    $module_name = $module_name[count($module_name) - 1];
+    $module_name = trim(str_replace(".git", "",$module_name ));
+    if(is_dir("./$module_name")){
+        deleteDir("./$module_name");
+    }
+    $clone_result = exec("git clone $module_url");
+
+    if(!is_dir("./$module_name")){
+        echo "Fetching error\n";
+        exit(1);
+    }
+}
 
 /**
  * Устанавливаем модуль с помощью git
@@ -35,24 +57,28 @@ function installModule($module_url, $updating=false, $check_tmp=true){
     if ($check_tmp) {
         checkRootPath();
     }
-
-    echo "$module_url installing\n";
-
-    $current_working_dir = getcwd();
-
-    if ($check_tmp) {
-          checkTmpFolder();
-    }
-    chdir(TMP_DIR);
-
-    $clone_result = exec("git clone $module_url");
-    
     $module_name = explode("/", $module_url);
     $module_name = $module_name[count($module_name) - 1];
     $module_name = trim(str_replace(".git", "",$module_name ));
     
     echo "module name = $module_name\n";
+    // $is_installed = ifModuleInstalled()
+    echo "$module_url installing\n";
+
+    $current_working_dir = CURRENT_WORKIN_DIR;
+
+    if ($check_tmp) {
+        checkTmpFolder();
+    }
+    chdir(TMP_DIR);
     
+    
+    
+    if(is_dir("./$module_name")){
+        deleteDir("./$module_name");
+    }
+    $clone_result = exec("git clone $module_url");
+
     if(!is_dir("./$module_name")){
         echo "Fetching error\n";
         exit(1);
@@ -67,6 +93,17 @@ function installModule($module_url, $updating=false, $check_tmp=true){
 
     $settings = json_decode(file_get_contents("./".SETTINGS_FILE), true);
     print_r($settings);
+    
+    if(array_key_exists('require', $settings)){
+        echo "Has dependings!!!";
+
+        $dependings = $settings['require'];
+
+        foreach($dependings as $depending){
+            installModule($depending);
+        } 
+    }
+    
     $vendor = $settings['vendor'] ?? "";
 
     if($vendor == ""){
@@ -105,17 +142,21 @@ function installModule($module_url, $updating=false, $check_tmp=true){
     }
     chdir($current_working_dir);
 
-    if(in_array('depends_on', $settings)){
-        echo "Has dependings!!!";
-
-        $dependings = $settings['depends_on'];
-
-        foreach($dependings as $depending){
-            installModule($depending);
-        } 
-    }
+    
 }
 
+function ifModuleInstalled($module_name_with_vendor){
+    $current_working_dir = CURRENT_WORKIN_DIR;
+    if (is_dir("./vendor/$module_name_with_vendor")) {
+        if(is_file("./vendor/$module_name_with_vendor/".SETTINGS_FILE)){
+            return MODULE_INSTALLED_AND_VINILLA;
+        } else {
+            return MODULE_INSTALLED_NOT_VINILLA;
+        }
+    } else {
+        return MODULE_NOT_INSTALLED;
+    }
+}
 function uninstallModule($module_name){
     checkRootPath();
     echo "$module_name uninstalling\n";
@@ -142,6 +183,19 @@ function updateModule($module_name){
     } else {
         echo "module is not installed!!!\nPlease run \n**********************\nvinilla_php install $module_name\n**********************\n";
     }
+}
+
+function updateCache(){
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_URL, SERVER_URL."/cache/get_all");
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    $server_output = curl_exec($ch);
+    echo "cache_json = $server_output\n";
+    curl_close ($ch);
+
 }
 // $longopts  = array(
 //     "install",     // Обязательное значение
@@ -173,5 +227,11 @@ for ($i=2; $i<$argc;$i++) {
             echo "Используй либо install либо uninstall либо update";
     }
 }
-
+if($argc <3){
+    switch($command) {
+        case "update":
+            updateCache();
+            break;
+    }
+}
 // print_r($options);
